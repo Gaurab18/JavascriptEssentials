1d77c3ce63cf845718a4693ccaf722e37d7be008
Gaurab18 <gaurab4218@gmail.com>
2024-12-12 13:05:38 +0530

    Updated Js file with User Experince text area

--- ProductSurvey/product_survey.js
+++ ProductSurvey/product_survey.js
@@ -3,6 +3,14 @@ const submitButton = document.getElementById('submitBtn')
 submitButton.onclick = submitFeedback;
 
 function submitFeedback(){
+
+    //since using event listener in js so required field doesnt worl therefore used this eventlistener.
+    document.getElementById('myForm').addEventListener('submit', function (event) {
+        if (!this.checkValidity()) {
+          event.preventDefault(); // Prevent submission if the form is invalid
+          alert('Please fill out the required fields.');
+        }
+
     const username = document.getElementById('name').value;
     const age = document.getElementById('age').value;
     const email = document.getElementById('email').value;
@@ -10,6 +18,7 @@ function submitFeedback(){
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experince = document.getElementById('experinceText').value;
 
     //update the content 
     document.getElementById('userName').innerHTML = username;
@@ -19,6 +28,8 @@ function submitFeedback(){
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+
+    document.getElementById('userExperince').innerHTML = experince;
 
     // Show the user info section
     document.getElementById('userInfo').style.display = 'block';
